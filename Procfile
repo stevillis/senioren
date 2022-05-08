bac67ebc8377514f076060c252311001c91c5ce3
Stévillis Sousa <stevillis@hotmail.com>
2022-05-08 01:21:34 -0400

    feature: heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn senioren.wsgi --log-file -
--- senioren/settings.py
+++ senioren/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
 import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -133,3 +135,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user.CustomUser'
+
+django_heroku.settings(locals())
