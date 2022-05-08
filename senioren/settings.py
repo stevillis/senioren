602791a0bef016a1a8c63610e39a17d5ba36c9dc
Stévillis Sousa <stevillis@hotmail.com>
2022-05-08 03:10:56 -0400

    feature: configure allowed hosts

--- senioren/settings.py
+++ senioren/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'senioren.herokuapp.com']
 
 # Application definition
 
