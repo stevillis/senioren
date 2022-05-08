885e9b6593f276c41cd18c4d81507badc9520312
Stévillis Sousa <stevillis@hotmail.com>
2022-05-08 03:09:18 -0400

    feature: configure gettext translation

--- senioren/settings.py
+++ senioren/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 import django_heroku
 import dotenv
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,6 +125,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'pt-br'
+
+LANGUAGES = (
+    ('pt-BR', _('Portuguese')),
+    ('en', _('English')),
+)
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
 
 TIME_ZONE = 'America/Cuiaba'
 
