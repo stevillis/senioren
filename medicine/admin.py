7aefa5565c607402a45a47ad64d6657d4b183171
Stévillis Sousa <stevillis@hotmail.com>
2022-05-09 02:20:29 -0400

    feature: add patient model to django admin

--- medicine/admin.py
+++ medicine/admin.py
@@ -1,7 +1,7 @@
 from daterangefilter.filters import DateRangeFilter
 from django.contrib import admin
 
-from medicine.models import Medicine, MedicalEvaluation
+from medicine.models import MedicalEvaluation, Medicine, Patient
 
 BASE_FIELDS = ['is_active', ]
 READ_ONLY_BASE_FIELDS = ['created_at', 'updated_at', ]
@@ -26,3 +26,21 @@ class MedicalEvaluationAdmin(admin.ModelAdmin):
         ('schedule', DateRangeFilter)
     ]
     # search_fields = [] # TODO: define searchable fields
+
+
+@admin.register(Patient)
+class MedicineAdmin(admin.ModelAdmin):
+    fields = ['name', 'social_name', 'cpf', 'rg', 'birth_date', 'marital_status', 'place_of_birth', 'gender', 'phone',
+              'observation'] + BASE_FIELDS
+    list_display = ('name', 'cpf', 'gender', 'marital_status', 'birth_date', 'phone',)
+    list_filter = [
+        'gender',
+        'marital_status',
+        ('birth_date', DateRangeFilter)
+    ]
+    search_fields = ('name', 'cpf', 'rg')
+
+    def save_model(self, request, obj, form, change):
+        if obj.gender == '':  # set default value for unselected gender
+            obj.gender = -1
+        super().save_model(request, obj, form, change)
