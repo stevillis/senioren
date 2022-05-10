698e65f4865d9ed9ddb8d313f481269417d9cba0
Stévillis Sousa <stevillis@hotmail.com>
2022-05-10 11:45:24 -0400

    style: code format

--- user/models.py
+++ user/models.py
@@ -8,30 +8,46 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(_('Name'), max_length=80, null=False, blank=False)
-    email = models.EmailField(_('Email'), unique=True, null=False, blank=False)
+    """CustomUser Model"""
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=80,
+        null=False,
+        blank=False
+    )
+    email = models.EmailField(
+        verbose_name=_('Email'),
+        unique=True,
+        null=False,
+        blank=False
+    )
     is_staff = models.BooleanField(
-        _('Staff status'), default=False,
-        help_text=_('Designates whether the user can log into this admin site.'),
+        verbose_name=_('Staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.')
     )
     is_active = models.BooleanField(
-        _('Active'),
+        verbose_name=_('Active'),
         default=True,
         help_text=_(
             'Designates whether this user should be treated as active. '
             'Unselect this instead of deleting accounts.'
         ),
     )
-    date_joined = models.DateTimeField(_('Date joined'), default=timezone.now)
+    date_joined = models.DateTimeField(
+        verbose_name=_('Date joined'),
+        default=timezone.now
+    )
 
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('name',)
 
-    def __str__(self):
+    def __str__(self):  # pylint: disable=invalid-str-returned
         return self.email
 
     class Meta:
+        """Metadata options"""
         verbose_name = _('User')
         verbose_name_plural = _('Users')
