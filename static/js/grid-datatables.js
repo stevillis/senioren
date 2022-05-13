a29d7bd5a78d64737acf600220f8be78d60f05ec
Stévillis Sousa <stevillis@hotmail.com>
2022-05-12 23:50:26 -0400

    feature: script for handle datatables data

---
+++ static/js/grid-datatables.js
@@ -0,0 +1,114 @@
+function getGridWrapper(tableName) {
+    return document.querySelector(`#${tableName}_wrapper`);
+}
+
+function addOrRemoveScroll(grid, limitWidth) {
+    const className = 'overflow-auto';
+    grid.clientWidth < limitWidth ? grid.classList.add(className) : grid.classList.remove(className);
+}
+
+function drag(tableName) {
+    /* https://codepen.io/thenutz/pen/VwYeYEE */
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+    const grid = getGridWrapper(tableName);
+
+    grid.addEventListener('mousedown', (e) => {
+        isDown = true;
+        grid.classList.add('active');
+        startX = e.pageX - grid.offsetLeft;
+        scrollLeft = grid.scrollLeft;
+    });
+
+    grid.addEventListener('mouseleave', () => {
+        isDown = false;
+        grid.classList.remove('active');
+    });
+
+    grid.addEventListener('mouseup', () => {
+        isDown = false;
+        grid.classList.remove('active');
+    });
+
+    grid.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - grid.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        grid.scrollLeft = scrollLeft - walk;
+    });
+}
+
+function addTableResizeEvent(tableName) {
+    const table = $(`#${tableName}`);
+
+    table.on('column-sizing.dt', function (e, settings) {
+        handleTableResizeEvent(tableName);
+    });
+
+    table.on('init.dt', function (e, settings) {
+        handleTableResizeEvent(tableName);
+        drag(tableName);
+    });
+}
+
+function handleTableResizeEvent(tableName) {
+    // Add horizontal scrollbar to grid
+    const gridWrapper = getGridWrapper(tableName);
+    switch (tableName) {
+        case 'gridMedicine':
+            addOrRemoveScroll(gridWrapper, 600);
+            break;
+    }
+}
+
+function initDataTable(grids) {
+    // Initialize Datatable according to the grid name
+    const pathName = '/static/assets/datatables/pt_br.json';
+
+    grids.forEach(gridName => {
+        const grid = document.querySelector(`#${gridName}`);
+
+        if (grid) {
+            const gridSize = grid.rows[0].cells.length;
+
+            const orderable = false;
+            const actionColumns = [
+                gridSize - 1, // History
+                gridSize - 2, // Deactivate
+                gridSize - 3, // Edit
+            ];
+            const searching = false;
+            const responsive = true;
+
+            switch (gridName) {
+                case 'gridMedicine':
+                    $(`#${gridName}`).DataTable( {
+                        columnDefs: [
+                            {
+                                orderable: orderable,
+                                targets: actionColumns,
+                            },
+                        ],
+                        language: {
+                            url: pathName
+                        },
+                        searching: searching,
+                        responsive: responsive,
+                    });
+
+                    addTableResizeEvent(gridName);
+                    break;
+            }
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+    const grids = [
+        'gridMedicine',
+    ]
+
+    initDataTable(grids);
+});
